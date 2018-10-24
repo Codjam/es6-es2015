18b88dd9f5f1d40bdd264def77ee22cf126175bc
Codjam <jamaltbutler>
2018-10-24 10:49:45 -0400

    ES6 arrow functions: Lexical 'this' keyword

--- script.js
+++ script.js
@@ -90,7 +90,7 @@
 
 //Lecture: Arrow Functions basic
 
-const years = [1990, 1965, 1982, 1976];
+// const years = [1990, 1965, 1982, 1976];
 
 //ES5
 // var ages5 = years.map(function(el) {
@@ -100,21 +100,109 @@ const years = [1990, 1965, 1982, 1976];
 
 
 //ES6 basic arrow functions
-let ages6 = years.map(el => 2016 - el);
-console.log(ages6);
-
-
-ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
-console.log(ages6);
-
-ages6 = years.map((el, index) => {
-  const now = new
-  Data().getFullYear;
-  const age = now - el;
-  return `Age element ${index + 1}:
-  ${age}.`
-});
-
-console.log(ages6);
+// let ages6 = years.map(el => 2016 - el);
+// console.log(ages6);
+//
+//
+// ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+// console.log(ages6);
+//
+// ages6 = years.map((el, index) => {
+//   const now = new
+//   Data().getFullYear;
+//   const age = now - el;
+//   return `Age element ${index + 1}:
+//   ${age}.`
+// });
+//
+// console.log(ages6);
 
 //Arrow functions: Lexical 'this Keyword'
+
+//ES5 cool trick (self instead of this)
+// var box5 = {
+//   color: 'green',
+//   position: 1,
+//   clickMe: function() {
+//
+//     var self = this;
+//     document.querySelector('.green').addEventListener('click',
+//     function() {
+//       var str = 'This is box number ' + self.position +
+//       ' and it is ' +
+//       self.color + '. ';
+//       alert(str);
+//     })
+//   }
+// }
+// box5 .clickMe();
+
+
+//ES6
+
+// const box6 = {
+//   color: 'green',
+//   position: 1,
+//   clickMe: function() {
+//
+//
+//     document.querySelector('.green').addEventListener('click',
+//     () => {
+//       var str = 'This is box number ' + this.position +
+//       ' and it is ' +
+//       this.color + '. ';
+//       alert(str);
+//     })
+//   }
+// }
+// box6 .clickMe();
+
+
+//not the right way to wright an arrow function.
+// const box66 = {
+//   color: 'green',
+//   position: 1,
+//   clickMe: () => {
+//
+//
+//     document.querySelector('.green').addEventListener('click',
+//     () => {
+//       var str = 'This is box number ' + this.position +
+//       ' and it is ' +
+//       this.color + '. ';
+//       alert(str);
+//     })
+//   }
+// }
+// box66 .clickMe();
+
+
+
+//ES5 cool trick (.bind)
+// function Person(name) {
+//   this.name = name;
+//
+// }
+// Person.prototype.myFriends5 =
+// function(friends) {
+//     var arr = friends.map(function(el)
+//   {
+//     return this.name + ' is friends with ' + el;
+//   }.bind(this));
+//   console.log(arr);
+// }
+//
+// var friends = ['Jamal, Joe, Jill'];
+// new Person('Jamila').myFriends5(friends);
+//
+//
+// //ES6 add on
+// Person.prototype.myFriends6 =
+// function(friends) {
+//
+//     var arr = friends.map(el =>
+//     `${this.name} is friends with
+//     ${el}`);
+//     console.log(arr);
+// }
+//  new Person('JamJam').myFriends6(friends);
